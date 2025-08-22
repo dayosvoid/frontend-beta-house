0ba6c40f898dd111c1222208bf533ec7ce60dd2a
Adedayo <atimileyin911@gmail.com>
2025-08-22 16:52:48 +0100

    Update Footer.jsx

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import logo from "../assets/logo.png"
 import message from "../assets/footer/message.png"
-import icon from "../assets/footer/icon.png"
+import icon from "../assets/icon.png"
 import call from "../assets/footer/call.png"
 
 
