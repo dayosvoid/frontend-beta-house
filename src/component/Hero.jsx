c9c205185de020d4ae2eabbe18eb3214a84f0c0d
Adedayo <atimileyin911@gmail.com>
2025-08-25 05:36:13 +0100

    made the search bar more dynamic
    made the search bar more dynamic

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Hero = () => {
+    const [bedRoom,setBedRoom]=useState(0)
+    const [error, setError]=useState('')
+
+    const bedRoomIncrement=()=>{
+        if(bedRoom === 10){
+           return setError('')
+        }
+        setBedRoom(bedRoom => bedRoom + 1)
+    }
+
+    const bedRoomDecrement=()=>{
+        if(bedRoom === 0){
+            return
+        }
+        setBedRoom(bedRoom => bedRoom - 1)
+    }
+
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+    }
+
   return (
     <div className='container mx-auto w-11/12 py-10'>
         {/* hero section */}
@@ -13,38 +34,42 @@ const Hero = () => {
             {/* details in the hero section */}
             <div className='w-full flex justify-center items-center'>
                 {/* the blur div */}
-                <div className='p-8  md:p-5 bg-white/20 w-full rounded-xl md:rounded-none flex flex-col md:flex-row'>
+                <div className='p-8  md:p-5 bg-white/20 w-full rounded-xl md:rounded-none flex flex-col md:flex-row '>
                     {/* the white div */}
-                    <div className='bg-white py-2 text-center flex flex-col md:flex-row md:justify-around rounded-t-md md:rounded-l-md w-full'>
-                        <span className='py-1  border-b md:border-none border-black'>
-                            <h1 className='font-semibold'>LOCATION</h1>
-                            <small>eg. Gbogada</small>
-                        </span>
+                    <form action="" onSubmit={handleSubmit} className='flex flex-col md:flex-row w-full'>
+                        <div className='bg-white py-2 text-center flex flex-col text-gray-600 md:flex-row md:justify-around rounded-t-md md:rounded-l-md w-full'>
+                            <span className='py-1  border-b md:border-none text-start px-5 w-full'>
+                                <h1 className='font-semibold text-gray-600'>LOCATION</h1>
+                                <input type="text" placeholder='eg. Gbogada' className='focus:outline-none text-[16px] w-full'/>
+                            </span>
 
+                            
+                            {/*the vertical line in the find property section  */}
+                                <div className='min-h-[80%] border border-gray-400 flex relative hidden md:flex '></div>
+
+                            <span className='py-1  border-b md:border-none text-gray-600 border-black text-start px-5 w-full'>
+                                <h1 className='font-semibold text-gray-600'>PROPERTY</h1>
+                                <input type="text" placeholder='eg. Duplex,bedroom Flat' className='focus:outline-none text-[16px] w-full '/>
+                            </span>
                         
-                        {/*the vertical line in the find property section  */}
-                            <div className='h-[80%] border-l border-black flex relative top-1.5'></div>
+                            {/*the vertical line in the find property section  */}
+                            <div className='min-h-[80%] border flex border-gray-400 relative hidden md:flex'></div>
 
-                        <span className='py-1  border-b md:border-none border-black'>
-                            <h1 className='font-semibold'>PROPERTY</h1>
-                            <small>eg. Duplex,bedroom Flat</small>
-                        </span>
-                    
-                        {/*the vertical line in the find property section  */}
-                        <div className='h-[80%] border-l border-black flex relative top-1.5'></div>
+                            <span className='py-1 flex flex-col justify-center text-gray-600 items-center px-5 relative md:w-full'>
+                                <h1 className='font-semibold pb-1 text-gray-600'>BEDROOM</h1>
+                                <div className='flex items-center text-center md:w-full justify-center gap-5 '>
+                                        <button onClick={bedRoomIncrement} className='border cursor-not-allowed border-2 border-black px-2 rounded-full'>+</button>
+                                        <p className=''>{bedRoom}</p>
+                                        <button onClick={bedRoomDecrement} className='border cursor-not-allowed border-2 border-black px-2 rounded-full self-center'>-</button>
+                                </div>
+                                {/* {error && <small className='absolute text-red-500 font-semibold bottom-[-8px]'>{error}</small>} */}
+                            </span>
 
-                        <span className='py-1  border-b md:border-none border-black flex flex-col justify-center items-center'>
-                            <h1 className='font-semibold pb-1'>BEDROOM</h1>
-                            <div className='flex justify-between items-center text-center w-20'>
-                                    <p className='border cursor-not-allowed border-2 border-black px-2 rounded-full'>+</p>
-                                    <p>0</p>
-                                    <p className='border cursor-not-allowed border-2 border-black px-2 rounded-full self-center'>-</p>
-                            </div>
-                        </span>
+                            
+                        </div>
+                        <button className='bg-[hsla(153,43%,42%,1)] cursor-not-allowed text-white font-semibold py-2 w-full md:w-[30%]'>Find Property</button>
+                    </form>
 
-                        
-                    </div>
-                    <button className='bg-[hsla(153,43%,42%,1)] cursor-not-allowed text-white font-semibold py-2 md:w-[30%]'>Find Property</button>
                 </div>
             </div>
         </div>
